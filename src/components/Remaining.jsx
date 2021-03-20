9010a4c0f207a66526db1f6a51bcf0bae53db239
John Mwaura <johnmwaura08@gmail.com>
2021-03-19 23:59:23 -0500

    add remaining expenses

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 function Remaining() {
+    const { expenses, budget } = useContext(AppContext);
+
+    console.log(expenses)
+    const testExpenses = [ 42, 402 , 52 ]
+    
+    testExpenses.reduce((total, item) => {
+        console.log(item)
+        console.log(total + item)
+        return total + item
+    }, 0)
+
+    // expenses.map(expense => (
+    //     console.log(expense.cost)
+    // )
+    // )
+     expenses.reduce((total, item) => {
+        let totali = total + item.cost
+        console.log(totali)
+        return totali
+    },0)
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
     return (
-        <div className="alert alert-success">
-            <span>Remaining: $1000</span>            
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totalExpenses}</span>
         </div>
     )
 }
