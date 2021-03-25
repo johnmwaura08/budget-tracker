59f7ffb410cba0b9a3117e8ca625711f321eb5a8
John Mwaura <johnmwaura08@gmail.com>
2021-03-25 07:51:43 -0500

    calculate total expenses using reduce function

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -2,7 +2,12 @@ import React,{useContext} from 'react'
 import { AppContext } from '../context/AppContext'
 
 function ExpenseTotal() {
-	const { totalExpenses} = useContext(AppContext)
+	const { expenses } = useContext(AppContext)
+    
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+
     return (
         <div className="alert alert-primary">
             <span>Spent so far: ${totalExpenses}</span>
