f697de8b654c8ef2da073ee95b82d3e2710d4c15
John Mwaura <johnmwaura08@gmail.com>
2021-03-25 07:49:05 -0500

    add icon for edit expense

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useContext,useEffect,useState } from 'react'
 import { TiDelete } from 'react-icons/ti'
+import { AiFillEdit} from 'react-icons/ai'
+import { AppContext } from '../context/AppContext';
 
 function ExpenseItem(props) {
+    const { dispatch, expenses } = useContext(AppContext);
+
+   
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        })
+    }
+    const handleEditExpense = (event) => {
+        console.log(selectedExpense)
+        event.preventDefault()
+        dispatch({
+            type: 'EDIT_EXPENSE',
+            payload: selectedExpense
+        })
+    }
     return (
       <li className="list-group-item d-flex justify-content-between align-items-center">{props.name}
       <div>
           <span className="badge badge-primary badge-pill mr-3">
               ${props.cost}
           </span>
-          <TiDelete size='1.5em'></TiDelete>
+          <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
+          <AiFillEdit size='1.5em' onClick={handleEditExpense}></AiFillEdit>
       </div>
       </li>
     )
